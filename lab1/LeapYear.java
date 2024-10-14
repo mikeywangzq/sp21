b41d0db2710adc8bd9e59fd29800e9762f4ceda6
WangMikey <592159453@qq.com>
2024-10-14 13:47:08 +0800

    completed first part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,19 @@ public class LeapYear {
         }
     }
 
+/** Determines whether or not a year is a leap year.
+ *  @param year to be analyzed
+ */
+    private static boolean isLeapYear(int year)
+    {
+        if(year%400 == 0)
+            return true;
+        else if(year%4 == 0 && year%100!=0)
+            return true;
+        else
+            return false;
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
